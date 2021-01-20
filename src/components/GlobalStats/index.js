6d94dea360e550a0937b3f1024a0d0a0683e39f1
TengYingWei <734970600@qq.com>
2021-01-21 01:51:45 +0800

    improve ui

--- src/components/GlobalStats/index.js
+++ src/components/GlobalStats/index.js
@@ -48,7 +48,7 @@ export default function GlobalStats() {
               }}
               style={{ position: 'relative' }}
             >
-              ETH Price: <Medium>{formattedEthPrice}</Medium>
+              HT Price: <Medium>{formattedEthPrice}</Medium>
               {/* {showPriceCard && <UniPrice />} */}
             </TYPE.main>
           )}
